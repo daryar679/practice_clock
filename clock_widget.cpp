4bc7d0da1aa4f8e3de056ac8725b83c3d67ee861
daryar679 <58885122+daryar679@users.noreply.github.com>
2020-12-26 06:40:44 +0300

    Update clock_widget.cpp

--- clock_widget.cpp
+++ clock_widget.cpp
@@ -5,67 +5,98 @@
 #include <iostream>
 
 Clock::Clock(QWidget* parent)
-	: QWidget(parent)
-	, _hours(0)
-	, _minutes(0)
-	, _seconds(0)
+    : QWidget(parent)
+    , _hours(0)
+    , _minutes(0)
+    , _seconds(0)
 {
-	setMinimumSize(100,100);
-	startTimer(1000);
+    setMinimumSize(100,100);
+    startTimer(1000);
 }
 
 void Clock::timerEvent(QTimerEvent*)
 {
-	QTime t  = QTime::currentTime();
-	_hours   = t.hour();
-	_minutes = t.minute();
-	_seconds = t.second();
+    QTime t  = QTime::currentTime();
+    _hours   = t.hour();
+    _minutes = t.minute();
+    _seconds = t.second();
 
-	update();
-	std::cout << "Tick: " 
-	<< _hours 
-	<< ":" << _minutes 
-	<< ":" << _seconds
-	<< std::endl;
+    update();
 }
 
 
 void drawArrow(QPainter& p)
 {
-	p.drawLine(0,0,  0,10);
-	p.drawLine(0,10,-3, 7);
-	p.drawLine(0,10, 3, 7);
+    p.drawLine(0,0,  0,10);
 }
 
 void Clock::paintEvent(QPaintEvent*)
 {
-	QPainter p(this);
-	QPoint center(width()/2, height()/2);
-	p.translate(center);
+    QPainter p(this);
+    QPoint center(width()/2, height()/2);
+    p.translate(center);
 
-/* Не очень красиво получилось - требуется другое решение
-	// hours
-	p.save();
-	p.rotate( 180 + _hours * 30);
-	p.scale(7,7);
-	p.setPen(Qt::red);
-	drawArrow(p);
-	p.restore();
+    //circle
+    p.setBrush(QBrush(Qt::black, Qt::SolidPattern));
+    p.drawEllipse(-153, -153, 300, 300);
+    for (int i = 0; i < 360; i+=6)
+    {
+        p.save();
+        p.setPen(Qt::white);
+        p.rotate(i);
+        if (i % 90 == 0)
+        {
+            p.scale(3,1);
+            p.drawLine(0,-153,0,-130);
+        }
+        if (i % 5 == 0)
+        {
+            p.scale(2,1);
+            p.drawLine(0,-153,0,-130);
+        }
+        else
+        {
+            p.drawLine(0,-153,0,-135);
+        }
+        p.restore();
+    }
 
-  // minutes
-	p.save();
-	p.rotate( 180 + _minutes * 6);
-	p.scale(18,18);
-	p.setPen(Qt::green);
-	drawArrow(p);
-	p.restore();
-*/
-  // seconds
-	p.save();
-	p.rotate( 180 + _seconds * 6);
-	p.scale(20,20);
-	p.setPen(Qt::blue);
-	drawArrow(p);
-	p.restore();
+    // hoursArrow
+    p.save();
+    p.rotate( 180 + _hours * 30);
+    p.scale(5,5);
+    p.setPen(QPen(Qt::white,0.7,Qt::SolidLine,Qt::RoundCap));
+    drawArrow(p);
+    p.restore();
+
+    // minutesArrow
+    p.save();
+    p.rotate( 180 + _minutes * 6);
+    p.scale(9,9);
+    p.setPen(QPen(Qt::white,0.5,Qt::SolidLine,Qt::RoundCap));
+    drawArrow(p);
+    p.restore();
+
+    // secondsArrow
+    p.save();
+    p.rotate( 180 + _seconds * 6);
+    p.scale(15,15);
+    p.setPen(QPen(Qt::blue,0.2,Qt::SolidLine,Qt::RoundCap));
+    drawArrow(p);
+    p.restore();
+
+    //numbers
+    p.setPen(Qt::white);
+    QFont mFont("Consolas",15,QFont::Bold);
+    p.setFont(mFont);
+    p.drawText(-14,-107,"12");
+    p.drawText(-7,117,"6");
+    p.drawText(100,7,"3");
+    p.drawText(-115,7,"9");
+
+    //time
+    p.setPen(Qt::black);
+    QFont nFont("Consolas",10,QFont::Bold);
+    p.setFont(mFont);
+    p.drawText(-60, 190,QString::number(_hours) +  ":" + QString::number(_minutes)+ ":" + QString::number(_seconds));
 }
-
